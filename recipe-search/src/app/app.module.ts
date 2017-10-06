d23f48ff490cd6787311719cd9429b4ea714b6be
rachd <rachel.m.dorn@gmail.com>
2017-10-05 22:22:44 -0400

    add firebase

--- recipe-search/src/app/app.module.ts
+++ recipe-search/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { appRouting } from './app.routing';
 import { CoreModule }   from './core/core.module';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDZPBbGmdplXyj3Z2mBaqXsFsSKY3gD33o",
+  authDomain: "recipe-search-72996.firebaseapp.com",
+  databaseURL: "https://recipe-search-72996.firebaseio.com",
+  projectId: "recipe-search-72996",
+  storageBucket: "recipe-search-72996.appspot.com",
+  messagingSenderId: "872814479005"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +27,8 @@ import { AppComponent } from './app.component';
     ReactiveFormsModule,
     HttpModule,
     CoreModule,
-    appRouting.routes
+    appRouting.routes,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
